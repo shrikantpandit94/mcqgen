fea6ef147ba0f0e770df44d32b0125d98d46339c
shrikantpandit94 <147649327+shrikantpandit94@users.noreply.github.com>
2024-07-23 04:08:10 +0000

    ...

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -5,7 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file,get_table_data
 import streamlit as st
-from langchain.callbacks import get_openai_callback
+from langchain_community.callbacks.manager import get_openai_callback
 from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
