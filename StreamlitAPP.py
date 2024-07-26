6c605cf665fd0088a4bcf53a3db41afdf2f00f4e
shrikantpandit94 <147649327+shrikantpandit94@users.noreply.github.com>
2024-07-26 04:24:57 +0000

    .

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -14,7 +14,7 @@ with open('/workspaces/mcqgen/Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
 
 #creating a title for the app
-st.title("MCQs Creator Application with LangChain 🦜⛓️")
+st.title("MCQs Creator Application with LangChain")
 
 #Create a form using st.form
 with st.form("user_inputs"):
